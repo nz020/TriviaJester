5a129edd16d80f6cfe38d0a7d9d0b6610896c2dd
Nikita Zaripov <nikita@MBP-von-Nikita.fritz.box>
2021-12-19 22:04:54 +0100

    set up Project TriviaKing

---
+++ TriviaKing/TriviaKing/MainScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriviaKing
+//
+//  Created by Nikita Zaripov on 19.12.21.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
