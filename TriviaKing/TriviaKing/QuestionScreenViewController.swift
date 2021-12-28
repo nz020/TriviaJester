08b6fa5add34cf71ea4418396c4705d53959e41e
Nikita Zaripov <nikita@MBP-von-Nikita.fritz.box>
2021-12-28 16:36:07 +0100

    create basic api call

--- TriviaKing/TriviaKing/QuestionScreenViewController.swift
+++ TriviaKing/TriviaKing/QuestionScreenViewController.swift
@@ -8,11 +8,67 @@
 import UIKit
 
 class QuestionScreenViewController: UIViewController {
+    
+    let url = "https://opentdb.com/api.php?amount=1";
+    
+    struct Response: Codable {
+        let response_code: Int
+        let results: [QuestionDetails]
+    }
+    
+    struct QuestionDetails: Codable {
+        let category: String
+        let type: String
+        let difficulty: String
+        let question: String
+        let correct_answer: String
+        let incorrect_answers: [String]
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        sendApiRequest(url: url)
 
         // Do any additional setup after loading the view.
+    }
+    
+    func sendApiRequest(url: String) {
+        
+        guard let url = URL(string: url) else {
+            print("Error: cannot create URL")
+            return
+        }
+        print(url)
+        //let urlRequest = URLRequest(url: url)
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: url, completionHandler: { data,response,error in
+            
+            guard let data = data, error == nil else {
+                print("error when fetching data")
+                return
+            }
+            
+            
+            var result: Response?
+            do {
+                result = try JSONDecoder().decode(Response.self, from: data)
+            }
+            catch {
+                print ("failed to decode data, Error: \(error)")
+            }
+            
+            guard let json = result else {
+                return
+            }
+            print (json.response_code)
+            print (json.results[0].category)
+            print (json.results)
+            
+        })
+        
+        task.resume()
     }
     
 
