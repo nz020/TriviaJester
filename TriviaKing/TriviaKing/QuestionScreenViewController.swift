5dd989808cd96d536b4302845701723302d365da
Nikita Zaripov <nikita@MBP-von-Nikita.fritz.box>
2021-12-29 20:51:54 +0100

    further implement QuestionScreenViewController

--- TriviaKing/TriviaKing/QuestionScreenViewController.swift
+++ TriviaKing/TriviaKing/QuestionScreenViewController.swift
@@ -9,8 +9,19 @@ import UIKit
 
 class QuestionScreenViewController: UIViewController {
     
-    let url = "https://opentdb.com/api.php?amount=1&category=";
-    let selectedCategory = "9" // test
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var difficultyLabel: UILabel!
+    @IBOutlet weak var firstAnswer: UIButton!
+    @IBOutlet weak var secondAnswer: UIButton!
+    @IBOutlet weak var thirdAnswer: UIButton!
+    @IBOutlet weak var fourthAnswer: UIButton!
+    
+    var overlay : UIView?
+    
+    
+    let url: String = "https://opentdb.com/api.php?amount=1&type=multiple&category=";
+    let selectedCategory: String = "9" // test
+    let currentQuestion: Int = 0;
     
     struct Response: Codable {
         let response_code: Int
@@ -29,9 +40,28 @@ class QuestionScreenViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        sendApiRequest(url: url)
+        //Overlay + loadingIndicator before Data is loaded
+        
+        overlay = UIView(frame: view.frame)
+        overlay!.backgroundColor = UIColor.white
+        overlay!.alpha = 1
+        
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(
+                                                        x: UIScreen.main.bounds.size.width*0.5,
+                                                        y: UIScreen.main.bounds.size.height*0.5,
+                                                        width: 50,
+                                                        height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.style = UIActivityIndicatorView.Style.medium
+        loadingIndicator.startAnimating();
 
-        // Do any additional setup after loading the view.
+        overlay?.addSubview(loadingIndicator)
+        view.addSubview(overlay!)
+
+        //API Call
+        
+        sendApiRequest(url: url)
+        
     }
     
     func sendApiRequest(url: String) {
@@ -44,13 +74,13 @@ class QuestionScreenViewController: UIViewController {
         //let urlRequest = URLRequest(url: url)
         let session = URLSession.shared
         
+        
         let task = session.dataTask(with: url, completionHandler: { data,response,error in
             
             guard let data = data, error == nil else {
                 print("error when fetching data")
                 return
             }
-            
             
             var result: Response?
             do {
@@ -64,12 +94,29 @@ class QuestionScreenViewController: UIViewController {
                 return
             }
             print (json.response_code)
-            print (json.results[0].category)
             print (json.results)
             
+            DispatchQueue.main.async {
+                self.questionLabel.text = json.results[self.currentQuestion].question
+                self.difficultyLabel.text = json.results[self.currentQuestion].difficulty
+                self.randomizePositionOfCorrectAnswer(correctAnswer: json.results[self.currentQuestion].correct_answer, wrongAnswers: json.results[self.currentQuestion].incorrect_answers)
+                self.overlay?.removeFromSuperview()
+            }
+
         })
-        
         task.resume()
+        
+    }
+    
+    func randomizePositionOfCorrectAnswer(correctAnswer: String, wrongAnswers: [String]) {
+        
+        var answersArray: [String] = [correctAnswer] + wrongAnswers;
+        answersArray.shuffle()
+        
+        self.firstAnswer.setTitle(answersArray[0], for: .normal)
+        self.secondAnswer.setTitle(answersArray[1], for: .normal)
+        self.thirdAnswer.setTitle(answersArray[2], for: .normal)
+        self.fourthAnswer.setTitle(answersArray[3], for: .normal)
     }
     
 
@@ -84,3 +131,4 @@ class QuestionScreenViewController: UIViewController {
     */
 
 }
+
